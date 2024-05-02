110a901edc8275c88f1288354465c2d4bca34dc2
AlessPal <alessandro.palumbi.pc@gmail.com>
2024-05-02 16:06:49 +0200

    allow credentials

--- backend/user_management/settings.py
+++ backend/user_management/settings.py
@@ -58,11 +58,6 @@ MIDDLEWARE = [
 	'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-]
-
 ROOT_URLCONF = 'user_management.urls'
 
 TEMPLATES = [
@@ -146,3 +141,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS settings
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
