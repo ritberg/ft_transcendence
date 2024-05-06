4a83df7a08977e2666601b5ce87e1f35e7ac3090
AlessPal <alessandro.palumbi.pc@gmail.com>
2024-05-06 12:29:43 +0200

    add default app config

--- backend/auth_user/apps.py
+++ backend/auth_user/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class AuthUserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auth_user'
+	name = 'auth_user'
+	verbose_name = 'Authentication and Authorization'
