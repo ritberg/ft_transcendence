8d22e55d5cead4df267201636dce55dbef717177
AlessPal <alessandro.palumbi.pc@gmail.com>
2024-05-13 12:09:59 +0200

    add default profile_picture

--- backend/auth_user/models.py
+++ backend/auth_user/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='profile_pics/default.jpg')
 
 	class Meta:
 		db_table = 'users'
