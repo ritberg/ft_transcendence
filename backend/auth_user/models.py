c4b3f67f622052f713b36255fe5b9f7da260ffb0
AlessPal <alessandro.palumbi.pc@gmail.com>
2024-05-13 15:33:10 +0200

    add constant for default profile picture

--- backend/auth_user/models.py
+++ backend/auth_user/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+DEFAULT_PROFILE_PICTURE = 'profile_pics/default.jpg'
+
 class CustomUser(AbstractUser):
-	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default='profile_pics/default.jpg')
+	profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True, default=DEFAULT_PROFILE_PICTURE)
 
 	class Meta:
 		db_table = 'users'
