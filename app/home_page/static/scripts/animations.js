fdd519294822ea40828a073ebaea3c750c359903
Diogo Ferreira Pombeiro <dferreir@c2r9s9.42lausanne.ch>
2024-05-14 17:45:39 +0200

    Made new correct animations for entry-screens + added/continued first tournament screen

--- app/home_page/static/scripts/animations.js
+++ app/home_page/static/scripts/animations.js
@@ -1,3 +1,5 @@
+import { GameMode } from './main.js';
+
 document.getElementById("hamburger-icon").addEventListener("click", function() {
 	document.getElementById("hamburger-icon").classList.toggle("active");
 	document.getElementById("vertical-tab").classList.toggle("active");
@@ -22,10 +24,56 @@ document.getElementById("profile-button").addEventListener("click", function() {
 	}
 });
 
-document.getElementById("online-mode").addEventListener("click", function() {
-	// const login_elements = document.getElementsByClassName("login-box");
-	// for (let i = 0; i < login_elements.length; i++)
-	// 	login_elements[i].style.display = "block";
-	document.getElementById("main-buttons").style.display = "none";
-	document.getElementById("login-box").style.display = "block";
+// document.getElementById("online-mode").addEventListener("click", function() {
+// 	// const login_elements = document.getElementsByClassName("login-box");
+// 	// for (let i = 0; i < login_elements.length; i++)
+// 	// 	login_elements[i].style.display = "block";
+// 	document.getElementById("main-buttons").style.display = "none";
+// 	document.getElementById("login-box").style.display = "block";
+// });
+
+document.getElementById("main-buttons").addEventListener("click", function() {
+	var button_id = event.target.id;
+	if (event.target.tagName.toLowerCase() === "button") {
+		document.getElementById("main-buttons").classList.add("hidden");
+		if (button_id === "online-mode") {
+			document.getElementById("login-box").style.display = "block";
+			document.getElementById("login-box").classList.add("shown");
+		}
+		else if (button_id === "tournoi-mode") {
+			document.getElementById("tournoi").style.display = "block";
+			document.getElementById("tournoi").classList.add("shown");
+		}
+		else if (button_id === "pvp-mode")
+			GameMode(0);
+		else if (button_id === "cpu-mode")
+			GameMode(1);
+		document.getElementById("main-buttons").addEventListener("animationend", function() {
+			document.getElementById("main-buttons").style.display = "none";
+		});
+	}
 });
+
+		// setTimeout(function() {
+		// 	document.getElementById("main-buttons").classList.remove("animated");
+		// 	document.getElementById("main-buttons").style.display = "none";
+		// 	if (event.target.id === "online-mode")
+		// 		document.getElementById("login-box").style.display = "block";
+		// }, 1000);
+	// }
+	// document.getElementById("main-buttons").classList.add("hidden");
+
+
+// document.getElementById("main-buttons").addEventListener("click", function() {
+// 	var button_id = event.target.id;
+// 	if (event.target.tagName.toLowerCase() === "button") {
+// 		document.getElementById("main-buttons").classList.add("hidden");
+// 		document.getElementById("main-buttons").addEventListener("transitionend", function() {
+// 			document.getElementById("main-buttons").style.display = "none";
+// 			if (button_id === "online-mode") {
+// 				document.getElementById("login-box").style.display = "block";
+// 				document.getElementById("login-box").classList.add("shown");
+// 			}
+// 		});
+// 	}
+// });
--- app/home_page/static/scripts/main.js
+++ app/home_page/static/scripts/main.js
@@ -6,18 +6,10 @@ export let delta = 1.5;
 export let loop_exec = 0;
 export let ai_activated = false;
 
-async function GameMode(n) {
-	//
-	if (n >= 2)
-		return;
-	//
-	document.getElementById("title").style.animationName = "none";
-	const button_elements = document.getElementsByClassName("gamemode-button");
+export async function GameMode(n) {
 	for (let i = 0; i < 40; i++)
 	{
 		delta /= 1.05;
-		const opacity = 0.5 - i / 40;
-		document.getElementById("main-buttons").style.opacity = opacity;
 		await sleep(20);
 	}
 	for (let i = 0; i < 20; i++)
@@ -37,10 +29,5 @@ async function GameMode(n) {
 		requestAnimationFrame(loop);
 	}
 }
-
-document.getElementById("pvp-mode").addEventListener("click", function() { GameMode(0); });
-document.getElementById("cpu-mode").addEventListener("click", function() { GameMode(1); });
-document.getElementById("tournoi-mode").addEventListener("click", function() { GameMode(2); });
-document.getElementById("online-mode").addEventListener("click", function() { GameMode(3); });
 
 stars(document.getElementById("game"));
