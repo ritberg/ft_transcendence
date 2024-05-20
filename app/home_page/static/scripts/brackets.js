b59e1cb9bdeaa2562ea2dc7f927c134bc6c55de8
Diogo Ferreira Pombeiro <dferreir@c2r9s9.42lausanne.ch>
2024-05-20 16:10:29 +0200

    Still working on first column bracket positionning

--- app/home_page/static/scripts/brackets.js
+++ app/home_page/static/scripts/brackets.js
@@ -1,16 +1,34 @@
 export function drawBrackets(n) {
 	// document.querySelector("#brackets");
+	// var curr_block = 0;
+	// for (let i = 0; i < n; i++) {
+	// 	const newDiv = document.createElement("div");
+	// 	const winHeight = parseInt(window.getComputedStyle(document.getElementById("brackets")).height, 10);
+	// 	const numBlocks = parseInt(document.getElementById("slider1").value) + (Math.round(document.getElementById("slider1").value / 3));
+	// 	newDiv.classList.add("rectangle-div");
+	// 	if (i % 2 == 0 && i > 0)
+	// 		curr_block++;
+	// 	if (i < 2)
+	// 		newDiv.style.boxShadow = `white 0 0 20px`;
+	// 	newDiv.style.top = `${(i + curr_block) * (winHeight / numBlocks)}px`;
+	// 	newDiv.textContent = `PLAYER ${i + 1}`; // Add numbering to each div (optional)
+	// 	document.querySelector("#brackets").appendChild(newDiv);
+	// }
 	for (let i = 0; i < n; i++) {
 		const newDiv = document.createElement("div");
-		const winHeight = parseInt(window.getComputedStyle(document.getElementById("brackets")).height, 10);
+		const winHeight = parseInt(window.getComputedStyle(document.getElementById("brackets")).height);
+		const blockHeight = winHeight / parseInt(document.getElementById("slider1").value);
+		const offset = winHeight / 2 - ((blockHeight * Math.round(parseInt(document.getElementById("slider1").value) / 2)) + (blockHeight * Math.round(parseInt(document.getElementById("slider1").value) / 2 + 1) - (blockHeight * Math.round(parseInt(document.getElementById("slider1").value) / 2)) / 2));
+		// const offset = blockHeight / 2;
+		// const offset = 0;
+		console.log(offset);
 		newDiv.classList.add("rectangle-div");
 		if (i < 2)
-			newDiv.style.boxShadow = `white 0 0 10px`;
-		if (i % 2 == 0 && i)
-			newDiv.style.top = `${i * (winHeight * 8 / 100) + 70}px`;
-		newDiv.style.top = `${i * (winHeight * 8 / 100) + 20}px`;
-		newDiv.style.left = `0px`;
-		newDiv.style.marginBottom = `${i * 5}px`;
+			newDiv.style.boxShadow = `white 0 0 20px`;
+		if (i % 2 == 0)
+			newDiv.style.top = `${i * blockHeight + offset}px`;
+		else
+			newDiv.style.top = `${(i - 1) * blockHeight + 70 + offset}px`;
 		newDiv.textContent = `PLAYER ${i + 1}`; // Add numbering to each div (optional)
 		document.querySelector("#brackets").appendChild(newDiv);
 	}
