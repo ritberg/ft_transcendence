b8fad368ee21d998c64d1b8636d685c2fa2a7d0a
mmakarov <mmakarov@student.42lausanne.ch>
2024-05-24 17:50:26 +0200

    24.05 ca marche pas

--- user_service/user_auth_system/user_management/models.py
+++ user_service/user_auth_system/user_management/models.py
@@ -17,7 +17,7 @@ class CustomUser(AbstractUser):
 	status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='offline')
 
 	class Meta:
-		db_table = 'auth_user'
+		db_table = 'users'
 
 	def __str__(self):
 		return self.username
