0b8c2a141d31b7362a78bfc555411be51b908671
Diogo Ferreira Pombeiro <dferreir@c3r1s3.42lausanne.ch>
2024-06-19 16:40:24 +0200

    Fixed animations

--- nginx/nginx/static/scripts/navigation.js
+++ nginx/nginx/static/scripts/navigation.js
@@ -6,138 +6,124 @@ import { displayProfile } from './stats.js';
 import { tournamentSettings } from './animations.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    /////////// CONTENT //////////////
-    const contentContainer = document.getElementById("content");
-    contentContainer.addEventListener("click", async function (event) {
-        let url = window.location.pathname;
-        if (event.target && event.target.id === "b-signin-ok") {
-            await loginButton(event);
-            if (userIsConnected == true)
-                route('/');
-        }
-        else if (event.target && event.target.id === "b-signup-ok") {
-            signupButton(event);
-        }
-        else if (event.target && event.target.id === "pvp-mode") {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route("/pvp/");
-        }
-        else if (event.target && event.target.id === "cpu-mode") {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route("/bot/");
-        }
-        else if (event.target && event.target.id === "tourney-mode") {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route("/tourney/");
-        }
-        else if (event.target && event.target.id === "online-mode") {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route("/online/");
-        }
-        // else if (event.target && event.target.id === "cpu-mode") {
-        //     document.getElementById("main-menu").classList.add("hidden");
-        //     GameMode(1);
-        // }
-        else if (event.target && event.target.id === "b-tourney_settings") {
-            tournamentSettings();
-        }
-        else if (event.target && event.target.id === "b-online-go"){
-            GameMode(3);
-        }
-        else if (event.target && event.target.id === "refresh-stats") {
-            // addGame();
-            displayProfile();
-        }
-        else if (event.target && event.target.id === "logout") {
-            logoutButton();
-            route("/");
-        }
-        else if (event.target && event.target.id === "profile") {
-            displayProfile();
-        }
-    });
+	/////////// CONTENT //////////////
+	const contentContainer = document.getElementById("content");
+	contentContainer.addEventListener("click", async function (event) {
+		let url = window.location.pathname;
+		console.log(url);
+		if (url == "/" && event.target.id !== "content") {
+			document.getElementById("content").classList.remove("shown");
+			document.getElementById("content").classList.add("hidden");
+			await sleep(500);
+		}
+		if (event.target && event.target.id === "b-signin-ok") {
+			await loginButton(event);
+			if (userIsConnected == true)
+				route('/');
+		}
+		else if (event.target && event.target.id === "b-signup-ok") {
+			signupButton(event);
+		}
+		else if (event.target && event.target.id === "pvp-mode") {
+			route("/pvp/");
+		}
+		else if (event.target && event.target.id === "cpu-mode") {
+			route("/bot/");
+		}
+		else if (event.target && event.target.id === "tourney-mode") {
+			route("/tourney/");
+		}
+		else if (event.target && event.target.id === "online-mode") {
+			route("/online/");
+		}
+		// else if (event.target && event.target.id === "cpu-mode") {
+		//     document.getElementById("main-menu").classList.add("hidden");
+		//     GameMode(1);
+		// }
+		else if (event.target && event.target.id === "b-tourney_settings") {
+			tournamentSettings();
+		}
+		else if (event.target && event.target.id === "b-online-go"){
+			GameMode(3);
+		}
+		else if (event.target && event.target.id === "refresh-stats") {
+			// addGame();
+			displayProfile();
+		}
+		else if (event.target && event.target.id === "logout") {
+			logoutButton();
+			route("/");
+		}
+		else if (event.target && event.target.id === "profile") {
+			displayProfile();
+		}
+		document.getElementById("content").classList.remove("hidden");
+		document.getElementById("content").classList.add("shown");
+		//document.getElementById("content").style.opacity = "1";
+	});
 
-    ////////// PROFILE ///////////
-    document.getElementById("profile_tab").addEventListener("click", async function (event){
-        event.preventDefault();
-        let url = window.location.pathname;
-        if (url === "/profile/" || url === "/signin/" || url === "/signup/")
-        {	
-            route('/');
-        }
-        else if (userIsConnected == true)
-        {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route('/profile/');
-            // await sleep(100);
-            // document.getElementById("user-info-box").style.opacity = "0";
-            // document.getElementById("user-info-box").classList.remove("hidden");
-            // document.getElementById("user-info-box").classList.add("shown");
-        }
-        else if (userIsConnected == false)
-        {
-            if (url == "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route('/signin/');
-        }
-        console.log("fdsafdsa", userIsConnected);
-    });
+	////////// PROFILE ///////////
+	document.getElementById("profile_tab").addEventListener("click", async function (event){
+		event.preventDefault();
+		let url = window.location.pathname;
+		console.log(url);
+		document.getElementById("content").classList.remove("shown");
+		document.getElementById("content").classList.add("hidden");
+		await sleep(500);
+		if (url === "/profile/")
+			route('/');
+		else if (url === "/signin/")
+			route('/');
+		else if (url === "/signup/")
+			route('/');
+		else if (userIsConnected == true)
+		{
+			if (url == "/") {
+				document.getElementById("content").classList.remove("shown");
+				document.getElementById("content").classList.add("hidden");
+			}
+			route('/profile/');
+		}
+		else if (userIsConnected == false)
+			route('/signin/');
+		document.getElementById("content").classList.remove("hidden");
+		document.getElementById("content").classList.add("shown");
+		console.log("fdsafdsa", userIsConnected);
+	});
 
-    ////////// USERS_LIST ///////////
-    document.getElementById("home-button").addEventListener("click", async function (event) {
-        route("/");
-    });
+	////////// USERS_LIST ///////////
+	document.getElementById("home-button").addEventListener("click", async function (event) {
+		document.getElementById("content").classList.remove("shown");
+		document.getElementById("content").classList.add("hidden");
+		await sleep(500);
+		route("/");
+		document.getElementById("content").classList.remove("hidden");
+		document.getElementById("content").classList.add("shown");
+	});
 
-    document.getElementById("users-full-list-button").addEventListener("click", async function (event) {					//// frontend
-        event.preventDefault();
-        if (window.location.pathname === "/users/") {
-            route("/");
-        }
-        else {
-            if (window.location.pathname === "/") {
-                document.getElementById("main-menu").classList.remove("shown");
-                document.getElementById("main-menu").classList.add("hidden");
-                await sleep(500);
-            }
-            route("/users/");
-        }
-    });
+	document.getElementById("users-full-list-button").addEventListener("click", async function (event) {					//// frontend
+		event.preventDefault();
+		document.getElementById("content").classList.remove("shown");
+		document.getElementById("content").classList.add("hidden");
+		await sleep(500);
+		if (window.location.pathname === "/users/")
+			route("/");
+		else
+			route("/users/");
+		document.getElementById("content").classList.remove("hidden");
+		document.getElementById("content").classList.add("shown");
+	});
 });
 
 document.getElementById("settings-button").addEventListener("click", async function (event) {					//// frontend
-    event.preventDefault();
-    if (window.location.pathname === "/settings/") {
-        route("/");
-    }
-    else {
-        if (window.location.pathname === "/") {
-            document.getElementById("main-menu").classList.remove("shown");
-            document.getElementById("main-menu").classList.add("hidden");
-            await sleep(500);
-        }
-        route("/settings/");
-    }
+	event.preventDefault();
+	document.getElementById("content").classList.remove("shown");
+	document.getElementById("content").classList.add("hidden");
+	await sleep(500);
+	if (window.location.pathname === "/settings/")
+		route("/");
+	else
+		route("/settings/");
+	document.getElementById("content").classList.remove("hidden");
+	document.getElementById("content").classList.add("shown");
 });
