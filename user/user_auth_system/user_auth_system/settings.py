2028dcbda216238b67e213b8975f3b85f53d121d
rrouille <rrouille@rrouille.local>
2024-06-19 22:02:52 +0200

    Add 2FA and JWT no display on frontend...

--- user/user_auth_system/user_auth_system/settings.py
+++ user/user_auth_system/user_auth_system/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('JWT_SERVICE_NAME')
+SECRET_KEY = env('JWT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
