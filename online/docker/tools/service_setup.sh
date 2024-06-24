191c3fa1c73a1740f1127c5b1e536d7ae72390e5
Joel Robert-Nicoud <joerober@c3r1s2.42lausanne.ch>
2024-06-24 13:31:07 +0200

    migrate fix + settings hide

--- online/docker/tools/service_setup.sh
+++ online/docker/tools/service_setup.sh
@@ -19,7 +19,7 @@ while ! nc -z db_transcendence 5434; do
   echo "waiting for postgress to be listening..."
   sleep 1
 done
-sleep 5
+sleep 10
 echo "PostgreSQL started"
 pip install -U 'Twisted[tls,http2]'
 python manage.py makemigrations
